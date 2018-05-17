c6b4bbc0ea267b1e086ff04ce4f6bb27d08a7c01
Elise Bonner <elise0209@mac.com>
2018-05-17 19:06:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var katzDeliLine = [];
 function takeANumber(katzDeliLine, newPerson) {
-  
+  katzDeliLine.push(newPerson);
+  return `Welcome, ${newPerson}. You are number ${katzDeliLine.length + 1} in line.`
+}
+
+function nowServing(){
+ if(katzDeliLine.length > 0){
+   var name = katzDeliLine[0];
+   katzDeliLine.shift();
+   return name;
+ }
+return "There is nobody waiting to be served!"
 }
